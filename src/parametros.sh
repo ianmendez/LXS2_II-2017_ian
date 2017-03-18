a2d754585ef90789fbe43937a5b7fb3f39465a57
ian Mendez A <alex29mendez@hotmail.com>
2017-03-18 16:54:00 -0600

    History de la clase 1

---
+++ src/parametros.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+argumentos=3
+
+if [ $# -eq $argumentos ]
+then
+	echo El numero de parametros fue correcto
+	if [ $1 = perro]
+	then
+		echo El parametro '$1' fue $1
+	fi
+else
+
+	echo El numero de parametros fue incorrecto
+	echo Se pasaron $# argumentos
+
+fi
+
+echo fin del script
+exit 0
